c9405e18209a5e872786234c7d36c096b006053c
Henrique Carvalho <carvalhoghenrique@gmail.com>
2025-07-17 10:32:28 -0300

    feat(area-activity): adiciona componente Area Activity com estrutura inicial

---
+++ src/app/Components/area-activity/area-activity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-activity',
+  standalone: false,
+  templateUrl: './area-activity.component.html',
+  styleUrl: './area-activity.component.css'
+})
+export class AreaActivityComponent {
+
+}
