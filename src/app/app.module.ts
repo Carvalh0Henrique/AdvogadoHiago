ca6274b30ee31fd361331e2aae6da20876bddf01
Henrique Carvalho <carvalhoghenrique@gmail.com>
2025-07-19 10:38:07 -0300

    feat(app.module.ts): Adiciona os componentes no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutComponent } from './Components/about/about.component';
+import { AreaActivityComponent } from './Components/area-activity/area-activity.component';
+import { BackgroundExperienceComponent } from './Components/background-experience/background-experience.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    AreaActivityComponent,
+    BackgroundExperienceComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
